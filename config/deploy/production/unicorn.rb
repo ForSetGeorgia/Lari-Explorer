6936106ec7e864b3e36236aaf9aa0a772a99b7f7
jason <jason.addie@gmail.com>
2017-06-13 13:52:43 +0400

    switch to use deploy user

--- config/deploy/production/unicorn.rb
+++ config/deploy/production/unicorn.rb
@@ -1,7 +1,7 @@
 ##################################
 ##### SET THESE VARIABLES ########
 ##################################
-root = "/home/lari/Lari-Explorer/current" # path to application current folder
+root = "/home/deploy/Lari-Explorer/current" # path to application current folder
 sock_name = "unicorn_Lari-Explorer" # must be unique name with no '.'
 port_num = 8136 # must be a unique port number for this application
 tout = 30 # time in seconds before process dies - need a long time for data uploads
