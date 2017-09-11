d98f00604a8b2453da7a90d5c64ddecd3db2de93
jason <jason.addie@gmail.com>
2017-09-11 12:26:38 +0400

    update deploy settings; deploy to work with ipv6

--- config/deploy/staging/unicorn.rb
+++ config/deploy/staging/unicorn.rb
@@ -1,8 +1,8 @@
 ##################################
 ##### SET THESE VARIABLES ########
 ##################################
-root = "/home/xrates-staging/Xrates-Staging/current" # path to application current folder
-sock_name = "unicorn_Xrates-Staging" # must be unique name with no '.'
+root = "/home/deploy/Lari-Explorer/current" # path to application current folder
+sock_name = "unicorn_Lari-Explorer" # must be unique name with no '.'
 port_num = 8136 # must be a unique port number for this application
 tout = 30 # time in seconds before process dies - need a long time for data uploads
 ##################################
